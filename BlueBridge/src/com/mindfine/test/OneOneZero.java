7596f951a91190a1cf81991f85d84134272e05f0
Lov Why <rankun203@gmail.com>
2013-04-11 20:52:00 +0800

    add project files.

--- BlueBridge/src/com/mindfine/test/OneOneZero.java
+++ BlueBridge/src/com/mindfine/test/OneOneZero.java
@@ -8,7 +8,30 @@ public class OneOneZero {
 
 	private static void sum110(String str, int start) {
 		String temp = str;
-		
+		if(str.indexOf("3") != -1){
+			System.out.println("expression: " + str);
+		}
+		for (int i=start; i<=3; i++) {
+			temp += "" + i;
+			sum110(temp, ++start);
+System.out.println("____:" + temp);
+			temp = str;
+			start = i;
+			
+			temp += "+" + i;
+			sum110(temp, ++start);
+System.out.println("++++:" + temp);
+			temp = str;
+			start = i;
+			
+			temp += "-" + i;
+			sum110(temp, ++start);
+System.out.println("----:" + temp);
+			temp = str;
+			start = i;
+
+			break;
+		}
 	}
 
 }

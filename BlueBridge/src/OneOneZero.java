e8283dec4c8e0a98f525a9bd1ad5a15e3a38a8f3
Lov Why <rankun203@gmail.com>
2013-04-11 18:41:53 +0800

    add file.

---
+++ BlueBridge/src/OneOneZero.java
@@ -0,0 +1,13 @@
+
+public class OneOneZero {
+
+	public static void main(String[] args) {
+		sum110("", 1);
+	}
+
+	private static void sum110(String str, int start) {
+		String temp = str;
+		
+	}
+
+}
